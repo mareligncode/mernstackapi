9f13c37e8dc3692fc2d6eeacf50f1bb619195f71
marelign <yimermarelign@gmail.com>
2025-10-24 21:55:04 +0300

    dev

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+const port = 5000
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}))
+app.get('/',(req,res)=> {
+    res.send("wellcome")
+})
+app.listen(port, () => {
+    console.log(`Serever runing in the port http://localhost:${port}`);
+    
+})
